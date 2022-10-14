d98d5dc56a3785af35e0a05edaab4bc1560473df
lmf <lmf@zenx.ltd>
2022-10-14 16:20:01 +0800

    SpringMVC创建控制器次数&RequestMapping注解的用法

--- springmvc-parent/mvc-01/src/main/java/com/wuman/FirstController.java
+++ springmvc-parent/mvc-01/src/main/java/com/wuman/FirstController.java
@@ -1,5 +1,6 @@
 package com.wuman;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,8 +16,16 @@ import javax.servlet.http.HttpServletResponse;
  * @date 2022/10/13 14:27
  */
 @Controller
+//@Scope("singleton")
+//@Scope("prototype") 次注解控制控制器创建的次数，singleton是一次，prototype是多次
 public class FirstController {
 
+    /**
+     * 注解RequestMapping可进行多路径指定/限定请求方式/省略第一层级的'/'
+     * @param request
+     * @param response
+     * @return
+     */
     @RequestMapping("/first")
     public String first(HttpServletRequest request, HttpServletResponse response){
         System.out.println("springmvc is running...");
